fa285b57b64e126c9b260fecae61c26c83de15fe
Victor Gabriel <vittimfkz@gmail.com>
2023-06-19 20:23:26 -0300

    Update GameActor.java

--- core/src/com/br/grupolaz/neocontra/actors/GameActor.java
+++ core/src/com/br/grupolaz/neocontra/actors/GameActor.java
@@ -1,24 +1,47 @@
 package com.br.grupolaz.neocontra.actors;
 
+import org.w3c.dom.Text;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Array;
+import com.br.grupolaz.neocontra.enums.ActorStates;
 import com.br.grupolaz.neocontra.util.Constants;
 
 
-//Inspired by MartianRun
+//Inspired by MartianRun & Brent Aureli
 public abstract class GameActor extends Actor {
     protected Body body;
     protected TextureRegion region;
     protected Sprite sprite;
+
+    //Sprite animation
+    protected ActorStates currentState;
+    protected ActorStates previousState;
+    protected TextureRegion actorStanding;
+    protected Animation<TextureRegion> actorRunning;
+    protected Animation<TextureRegion> actorJumping;
+    protected Animation<TextureRegion> actorCrounching;
+
+    protected boolean runningRight;
+    protected float stateTimer;
 
     public GameActor(Body body, TextureRegion region) {
         this.body = body;
         this.sprite = new Sprite(region);
         this.sprite.setSize(12.5f / Constants.PIXELS_PER_METER, 18f / Constants.PIXELS_PER_METER);
         this.region = new TextureRegion(region, 0, 0, 25, 36);
+
+        Array<TextureRegion> frames = new Array<TextureRegion>();
+
+        currentState = previousState = ActorStates.STANDING;
+        stateTimer = 0;
+        runningRight = true;
     }
 
     public void setDrawRegion(int x, int y, int width, int height) {
