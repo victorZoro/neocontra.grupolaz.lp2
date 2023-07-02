a65e3a53e3fcfc41de336182e78b3704783b5618
Lucas <trabalhobtmog123@gmail.com>
2023-07-02 09:37:19 -0300

    Dicumentação  dos arquivos da pasta enums

--- core/src/com/br/grupolaz/neocontra/enums/ActorStates.java
+++ core/src/com/br/grupolaz/neocontra/enums/ActorStates.java
@@ -1,5 +1,24 @@
 package com.br.grupolaz.neocontra.enums;
-
+/**
+ * <h2>ActorStates</h2>
+ * <p> a função geral da enumeração
+ *  ActorStates é fornecer uma representação 
+ * clara e organizada dos estados possíveis 
+ * de um ator em um jogo, facilitando o controle
+ *  e o gerenciamento do comportamento do ator
+ *  em diferentes situações.</p>
+ * 
+ * <h3>package</h3>
+ * <p>enums</p>
+ * 
+ * <h3>estados presentes na enumeração</h3>
+ * <p>FALLING</p>
+ * <p>JUMPING</P>
+ * <p>RUNNING</p>
+ * <p>STANDING</p>
+ * <p>CROUCHING</p>
+ * <p>AIMING_UP</p>
+ */
 public enum ActorStates {
     FALLING,
     JUMPING,
--- core/src/com/br/grupolaz/neocontra/enums/GameState.java
+++ core/src/com/br/grupolaz/neocontra/enums/GameState.java
@@ -1,5 +1,22 @@
 package com.br.grupolaz.neocontra.enums;
 
+/**
+ * <h2>GameState</h2>
+ * <p>A enumeração GameState é define os
+ *  possíveis estados do jogo.
+ * <p> Ela forneceuma maneira de representar 
+ * e distinguirentre os diferentes estados em
+ *  que o jogo pode estar, como "em execução", 
+ * "pausado" ou "terminado". </P>
+ * 
+ * <h3>package</h3>
+ * <p>enums</p>
+ * 
+ * <h3>Estados presentes na enumeração</h3>
+ * <P> RUNNING</p>
+ * <p>PAUSED</p>
+ * <p>OVER</p>
+ */
 public enum GameState {
     RUNNING,
     PAUSED,
