839093f434efd97d57e40bfc7a90a37fbcc5eaa8
Victor Gabriel <victorgabmariano@gmail.com>
2023-07-03 18:19:35 -0300

    Fixing update problem.

--- core/src/com/br/grupolaz/neocontra/screens/GameScreen.java
+++ core/src/com/br/grupolaz/neocontra/screens/GameScreen.java
@@ -65,8 +65,8 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        gameStage.draw();
         gameStage.act(delta);
+        gameStage.draw();
     }
 
 
