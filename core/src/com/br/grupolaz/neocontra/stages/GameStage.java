d679bf8d0b0e59da5f4ef8a486846a69b78c0f55
Anna Luisa <annaluisa1122@gmail.com>
2023-08-10 15:16:07 -0300

    Update GameStage.java

--- core/src/com/br/grupolaz/neocontra/stages/GameStage.java
+++ core/src/com/br/grupolaz/neocontra/stages/GameStage.java
@@ -1,7 +1,7 @@
 package com.br.grupolaz.neocontra.stages;
 
 
-import com.badlogic.gdx.Gdx;
+
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.scenes.scene2d.Stage;
