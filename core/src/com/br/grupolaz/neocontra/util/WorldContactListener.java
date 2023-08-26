0e79b100cab2cb76cff6624e0720e9595fbfdc4c
Lucas <trabalhobtmog123@gmail.com>
2023-08-26 14:58:15 -0300

    mudança

--- core/src/com/br/grupolaz/neocontra/util/WorldContactListener.java
+++ core/src/com/br/grupolaz/neocontra/util/WorldContactListener.java
@@ -1,32 +1,51 @@
 package com.br.grupolaz.neocontra.util;
 
-import com.badlogic.gdx.physics.box2d.*;
-import com.br.grupolaz.neocontra.actors.GameActor;
+<<<<<<<Updated upstream
 
+import com.badlogic.gdx.physics.box2d.*;=======
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.Manifold;
+import com.br.grupolaz.neocontra.actors.GameActor;
+import com.br.grupolaz.neocontra.actors.Player;
 
 public class WorldContactListener implements ContactListener {
 
     @Override
     public void beginContact(Contact contact) {
-        Fixture fixtureA = contact.getFixtureA();
-        Fixture fixtureB = contact.getFixtureB();
+    /**
+     * Fixture fixtureA = contact.getFixtureA();
+     * Fixture fixtureB = contact.getFixtureB();
+     * 
+     * if((fixtureA.getUserData() == "bullet") || (fixtureB.getUserData() ==
+     * "bullet")) {
+     * Fixture bullet = fixtureA.getUserData() == "bullet" ? fixtureA : fixtureB;
+     * Fixture enemy = bullet == fixtureA ? fixtureB : fixtureA;
+     * 
+     * System.out.println(enemy.getUserData());
+     * 
+     * if(enemy.getUserData() != null &&
+     * GameActor.class.isAssignableFrom(enemy.getUserData().getClass())) {
+     * ((GameActor) enemy.getUserData()).collision();
+     * }
+     * <<<<<<< Updated upstream
+     * }
+     * 
+     * =======
+     * }
+     */
+    >>>>>>>
 
-        if((fixtureA.getUserData() == "bullet") || (fixtureB.getUserData() == "bullet")) {
-            Fixture bullet = fixtureA.getUserData() == "bullet" ? fixtureA : fixtureB;
-            Fixture enemy = bullet == fixtureA ? fixtureB : fixtureA;
-
-            System.out.println(enemy.getUserData());
-
-            if(enemy.getUserData() != null && GameActor.class.isAssignableFrom(enemy.getUserData().getClass())) {
-                ((GameActor) enemy.getUserData()).collision();
-            }
-        }
-
+    Stashed changes
     }
 
     @Override
     public void endContact(Contact contact) {
-//        Gdx.app.log("End Contact", "test");
+        // Gdx.app.log("End Contact", "test");
     }
 
     @Override
@@ -36,5 +55,5 @@ public class WorldContactListener implements ContactListener {
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
     }
-    
+
 }
